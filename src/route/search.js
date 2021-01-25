439552f660e0ab6a7626c49a5d8fc0a588d2219b
Ashutosh <adixit.ashu@gmail.com>
2021-01-26 03:53:15 +0530

    m

--- src/route/search.js
+++ src/route/search.js
@@ -2,10 +2,10 @@ const route = require('express').Router()
 const connection = require('../sqldb').connection
 
 route.get('/', (req, res) => {
-    const query = 'select * from product where product_name like %?%'
+    const query = 'select * from product where product_name like ???'
     connection.query(
         query,
-        [req.query.q],
+        ['%', req.query.q, '%'],
         function (err, results) {
             res.json(results || err);
         }
