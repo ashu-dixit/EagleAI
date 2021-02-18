b5839a6f8dca8d73eec30cc7b9f63e45280cf5dc
Ashutosh <adixit.ashu@gmail.com>
2021-02-18 12:50:52 +0530

    m

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ const express = require('express');
 const path = require('path')
 const PORT = process.env.PORT || 8080
 const authcheck = require('./route/authmiddleware').authcheck
-
+const morgan = require('morgan')
 const app = express();
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+
+app.use(morgan('tiny')) 
 
 //refering to our static app in public folder
 app.use('/', express.static(path.join(__dirname, 'public')));
