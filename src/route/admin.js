7eff052f16956172c263634893d00b7ed1b74e3b
Ashutosh <adixit.ashu@gmail.com>
2021-02-18 11:14:39 +0530

    m

--- src/route/admin.js
+++ src/route/admin.js
@@ -67,7 +67,7 @@ route.patch('/products', (req, res) => {
     const query = `UPDATE product set Vendor_ID = ?, product_name = ?, product_price = ?, max_product_qty = ?, expiry_date =  STR_TO_DATE(?, "%Y-%m-%d"), product_image = ?, discount = ?, category = ?, WHERE Product_ID = ?;`
     connection.query(
         query,
-        [res.body.Vendor_ID, req.body.product_name, req.body.product_price, req.body.max_product_qty, req.body.expiry_date, req.body.product_image, req.body.discount, req.body.category, req.body.Product_ID],
+        [req.body.Vendor_ID, req.body.product_name, req.body.product_price, req.body.max_product_qty, req.body.expiry_date, req.body.product_image, req.body.discount, req.body.category, req.body.Product_ID],
         function (err, results) {
             res.send(results || err);
         }
